0686682d59f60142641b06336b70d4e85573519f
John Zhang <johnhaoxian@gmail.com>
2020-07-13 16:04:39 -0400

    fixing address display problem

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const apiKey = "gh5LtaMRnt6aKmPs1FMCJK3X8gwxhjjuTx3dUxMD"
 
 const searchURL = 'https://developer.nps.gov/api/v1/places';
-
+const addressURL = 'https://developer.nps.gov/api/v1/visitorcenters'
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
@@ -22,7 +22,7 @@ function displayResults(responseJson, maxResults) {
     //list with the article title, source, author,
     //description, and image
     $('#results-list').append(
-      `<li><h3><a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a></h3>
+      `<li id = "location${i}" ><h3><a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a></h3>
       <h4>${responseJson.data[i].title}</h4>
       <h5>${responseJson.data[i].listingDescription}</h4>
       <p>${responseJson.data[i].bodyText}</p>
@@ -31,19 +31,34 @@ function displayResults(responseJson, maxResults) {
   //display the results section  
   $('#results').removeClass('hidden');
 };
+//address object to html display
 
+function displayAddress (responseJson, maxResults){
+     // if there are previous results, remove them
+  console.log(responseJson,maxResults);
+  const number = parseFloat(maxResults);
+  // iterate through the articles array, stopping at the max number of results
+  for (let i = 0; i < responseJson.data.length & i<number ; i++){
+    //adding address
+
+    $(`#location${i}`).append(
+      `
+      
+      <h5>Address: ${responseJson.data[i].addresses[1].line1} ${responseJson.data[i].addresses[1].postalCode}</h5>
+      
+      `
+    )};
+    $('#results').removeClass('hidden');
+};
 function getNews(query, maxResults=10) {
   const params = {
     stateCode: query,
     limit: maxResults
   };
-  const queryString = formatQueryParams(params)
+  const queryString = formatQueryParams(params);
   const url = searchURL + '?' + queryString+ '&api_key=' +apiKey;
-
   console.log(url);
-
-  
-  fetch(url)
+  return fetch(url)
     .then(response => {
       if (response.ok) {
         return response.json();
@@ -54,14 +69,38 @@ function getNews(query, maxResults=10) {
     .catch(err => {
       $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
-}
+};
+function getAddress(query, maxResults=10) {
+    const params = {
+        stateCode: query,
+        limit: maxResults
+      };
+      const queryString = formatQueryParams(params);
+      const addURL = addressURL +'?' + queryString+ '&api_key=' +apiKey;
+      console.log(addURL);
+
+     fetch(addURL)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayAddress(responseJson, maxResults))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });  
+ }
+    
+ 
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
     const searchTerm = $('#js-search-term').val();
     const maxResults = $('#js-max-results').val();
-    getNews(searchTerm, maxResults);
+    getNews(searchTerm, maxResults).then(() => getAddress(searchTerm, maxResults));
+    
   });
 }
 
